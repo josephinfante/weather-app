c6353385c96e92573595e0af74541d641522c69d
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2023-08-11 00:23:50 -0500

    fixing displaying empty state, when there is no data

--- src/App.tsx
+++ src/App.tsx
@@ -21,31 +21,33 @@ function App() {
             }}/>
           </form>
         </div>
-        <div className="flex justify-center items-center flex-col lg:flex-row lg:items-start lg:justify-between">
-          <div className="text-center mb-5 lg:text-left lg:mb-0">
-            <div className="flex items-center text-lg mb-4 justify-center lg:justify-start"><span className="mr-3">{Icons('location')}</span><p>{weather.location.name}, {weather.location.country}</p></div>
-            <p className="text-5xl font-extrabold">{formatDate(weather.location.localtime)}</p>
-            <p className="text-xl font-semibold mt-3">{getDay(weather.location.localtime)}</p>
-          </div>
-          <div className="w-[200px] lg:w-[300px] h-auto">{BigImages(weather.current.condition.text, getState(weather.location.localtime))}</div>
-          <div className="w-full lg:w-auto">
-            <div className="flex items-center mt-5 lg:mt-0 justify-center lg:justify-start">
-              <div className="h-[50px] w-auto"><img src="./src/assets/thermometer.webp" alt="thermometer" width={"100%"} height={"100%"}/></div>
-              <p className="text-4xl font-extrabold ml-3">{weather.current.temp_c}° C</p>
+        {weather.location.name && (
+          <div className="flex justify-center items-center flex-col lg:flex-row lg:items-start lg:justify-between">
+            <div className="text-center mb-5 lg:text-left lg:mb-0">
+              <div className="flex items-center text-lg mb-4 justify-center lg:justify-start"><span className="mr-3">{Icons('location')}</span><p>{weather.location.name}, {weather.location.country}</p></div>
+              <p className="text-5xl font-extrabold">{formatDate(weather.location.localtime)}</p>
+              <p className="text-xl font-semibold mt-3">{getDay(weather.location.localtime)}</p>
             </div>
-            <div className="flex items-center justify-between lg:block">
-              <div>
-                <p className="my-5 text-sm">Pressure, mb <span className="ml-5 text-md font-semibold">{weather.current.pressure_mb}</span></p>
-                <p className="my-5 text-sm">Humidity, % <span className="ml-5 text-md font-semibold">{weather.current.humidity}</span></p>
+            <div className="w-[200px] lg:w-[300px] h-auto">{BigImages(weather.current.condition.text, getState(weather.location.localtime))}</div>
+            <div className="w-full lg:w-auto">
+              <div className="flex items-center mt-5 lg:mt-0 justify-center lg:justify-start">
+                <div className="h-[50px] w-auto"><img src="./src/assets/thermometer.webp" alt="thermometer" width={"100%"} height={"100%"}/></div>
+                <p className="text-4xl font-extrabold ml-3">{weather.current.temp_c}° C</p>
               </div>
-              <div>
-                <p className="my-5 text-sm">Cloud, % <span className="ml-5 text-md font-semibold">{weather.current.cloud}</span></p>
-                <p className="my-5 text-sm">Wind, kph <span className="ml-5 text-md font-semibold">{weather.current.wind_kph}</span></p>
+              <div className="flex items-center justify-between lg:block">
+                <div>
+                  <p className="my-5 text-sm">Pressure, mb <span className="ml-5 text-md font-semibold">{weather.current.pressure_mb}</span></p>
+                  <p className="my-5 text-sm">Humidity, % <span className="ml-5 text-md font-semibold">{weather.current.humidity}</span></p>
+                </div>
+                <div>
+                  <p className="my-5 text-sm">Cloud, % <span className="ml-5 text-md font-semibold">{weather.current.cloud}</span></p>
+                  <p className="my-5 text-sm">Wind, kph <span className="ml-5 text-md font-semibold">{weather.current.wind_kph}</span></p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div ref={containerRef} className="flex justify-between items-center w-full overflow-x-auto h-[200px]"><WeatherCard Forecast={weather.forecast}/></div>
+        )}
+        { weather.location.name && <div ref={containerRef} className="flex justify-between items-center w-full overflow-x-auto h-[200px]"><WeatherCard Forecast={weather.forecast}/></div>}
       </section>
       { loading && <LoadingSpinner />}
     </>
